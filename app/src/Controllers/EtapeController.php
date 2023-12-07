5e2bd6b9d66f3318359aded5bcba32dab2956ca0
Gabriel <60087391+g4bey@users.noreply.github.com>
2023-12-08 00:08:48 +0100

    Create EtapeController.php

---
+++ app/src/Controllers/EtapeController.php
@@ -0,0 +1,29 @@
+<?php
+    
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Utils\DB;
+use PDO;
+
+
+class EtapeController extends BaseController
+{
+    public function getEtapes(Request $request, Response $response, $args) {
+        $id = $args['id'];
+        
+        $conn = $this->db->connect();
+        $stmt = $conn->prepare("SELECT * FROM Etape WHERE idEtape = :idEtape");
+        $stmt->execute([':idEtape' => $id]);
+        $etapes = $stmt->fetchAll(PDO::FETCH_OBJ);
+        
+        $response->getBody()->write(json_encode($etapes));
+ 
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+
+    }
+
+}
