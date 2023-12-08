7b7d564b0feab6402d6a0fedead6ce39810424ff
MalauryHn <malaury.huon@gmail.com>
2023-12-08 05:59:37 +0100

    ajout easter eggs

---
+++ app/public/js/konami.js
@@ -0,0 +1,31 @@
+var allowedKeysKonami = {
+    37: 'gauche',
+    38: 'haut',
+    39: 'droite',
+    40: 'bas',
+    65: 'a',
+    66: 'b'
+};
+
+var konami = ['haut', 'haut', 'bas', 'bas', 'gauche', 'droite', 'gauche', 'droite', 'b', 'a'];
+
+var codePositionKonami = 0;
+
+document.addEventListener('keydown', function(e) {
+    var keyKonami = allowedKeysKonami[e.keyCode];
+    var requiredKeyKonami = konami[codePositionKonami];
+
+    if (keyKonami == requiredKeyKonami) {
+      codePositionKonami++;  
+      if (codePositionKonami == konami.length) {
+        successKonami();
+        codePositionKonami = 0;
+      }
+    } else {
+      codePositionKonami = 0;
+    }
+});
+
+function successKonami() {
+  window.alert('test');
+}
